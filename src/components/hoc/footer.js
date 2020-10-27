168aee25f599be51d9aeb02a999c79f800d591c3
LilyanAhmetoglu <bayanxinon@gmail.com>
2020-10-27 11:11:02 +0300

    addimg footer

---
+++ src/components/hoc/footer.js
@@ -0,0 +1,55 @@
+import React from "react"
+import { Container, Row, Col } from "react-bootstrap"
+
+/* reources*/
+import Icon from "../../../static/favicon.ico"
+import Facebook from "../../images/001-facebook.svg"
+import Instagram from "../../images/002-instagram.svg"
+import Twitter from "../../images/003-twitter.svg"
+import Seperator from '../../images/separator.svg'
+const Footer = () => {
+  return (
+    <div className="footer">
+      <Container>
+          <Row>
+              <img src={Seperator} style={{paddingBottom:"30px",width:"100%"}}/>
+          </Row>
+        <Row>
+          <Col xs={12} md={3}>
+            <div className="left">
+              <p>
+                © {new Date().getFullYear()} Copyright
+                {` `}
+                <a href="https://www.qimia.io">KWICKI</a>
+              </p>
+            </div>
+          </Col>
+          <Col xs={12} md={6}>
+            <div className="middle">
+              <img src={Icon} />
+              <div className="bottom">
+                <a href="#">Impressum</a>
+                <a href="#">Privacy Policy</a>
+              </div>
+            </div>
+          </Col>
+          <Col xs={12} md={3}>
+            <div className="social">
+              <a href="#">
+                <img src={Facebook} />
+              </a>
+              <a href="#">
+                <img src={Instagram} />
+              </a>
+              <a href="#">
+                <img src={Twitter} />
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+}
+
+export default Footer
